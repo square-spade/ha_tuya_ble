a7ee1c0ed9f2c41481c9152f282781802d05f66d
square-spade <41908243+square-spade@users.noreply.github.com>
2025-06-15 12:48:21 +1000

    Update number.py

--- custom_components/tuya_ble/number.py
+++ custom_components/tuya_ble/number.py
@@ -542,7 +542,7 @@ mapping: dict[str, TuyaBLECategoryNumberMapping] = {
     "sfkzq": TuyaBLECategoryNumberMapping(
         products={
             **dict.fromkeys(
-                ["46zia2nz", "1fcnd8xk", "0axr5s0b", "svhikeyq"],
+                ["46zia2nz", "1fcnd8xk", "0axr5s0b",],
                 [
                     TuyaBLENumberMapping(
                         dp_id=11,
@@ -550,9 +550,34 @@ mapping: dict[str, TuyaBLECategoryNumberMapping] = {
                             key="countdown_duration",
                             icon="mdi:timer",
                             native_max_value=86400,
-                            native_min_value=0,
+                            native_min_value=1,
                             native_unit_of_measurement=UnitOfTime.SECONDS,
                             native_step=1,
+                        ),
+                    ),
+                ],
+            ),
+            "svhikeyq": [
+                TuyaBLENumberMapping(
+                    dp_id=11,
+                    description=NumberEntityDescription(
+                        key="countdown",
+                        icon="mdi:timer",
+                        native_max_value=86400,
+                        native_min_value=1,
+                        native_unit_of_measurement=UnitOfTime.SECONDS,
+                        native_step=1,
+                        ),
+                    ),
+                TuyaBLENumberMapping(
+                    dp_id=9,
+                    description=NumberEntityDescription(
+                        key="countdown_duration",
+                        icon="mdi:timer",
+                        native_max_value=86400,
+                        native_min_value=1,
+                        native_unit_of_measurement=UnitOfTime.SECONDS,
+                        native_step=1,
                         ),
                     ),
                 ],
